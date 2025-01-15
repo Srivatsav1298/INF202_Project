67186af57723833a22702825bb76392c74f331fe
Srivatsav <abbuvatsav1298@gmail.com>
2025-01-15 12:32:32 +0100

    Modified Main.py with the incoming changes

--- OilSimulateProject/main.py
+++ OilSimulateProject/main.py
@@ -1,9 +1,5 @@
 import time
 from src.io.mesh_reader import Mesh
-<<<<<<< HEAD
-from src.visualization.plotter import plot_mesh, create_animation
-=======
->>>>>>> origin/main
 from src.simulation.simulator import Simulation
 
 oil_spill_center = (0.35, 0.45)
@@ -18,30 +14,6 @@ def main():
     # Create and setup the mesh
     file_name = "data/mesh/bay.msh"
     bay_mesh = Mesh(file_name)
-<<<<<<< HEAD
-    bay_mesh.find_neighbours()
-    
-    # Create simulation
-    oil_spill_simulation = Simulation(
-        bay_mesh, 
-        oil_spill_center=(0.35, 0.45),
-        end_time=2.0,  # Simulate for 2 time units
-        dt=0.01
-    )
-    
-    # Initialize
-    oil_spill_simulation.initialize_oil_spill()
-    
-    # Plot initial state
-    print("Plotting initial state...")
-    plot_mesh(bay_mesh)
-    
-    # Create and save animation
-    print("Creating animation...")
-    create_animation(oil_spill_simulation, num_frames=200)
-    
-    end_time = time.time()
-=======
 
     # Build neighbour relationships for each cell and related outward normal
     bay_mesh.find_neighbours()
@@ -52,7 +24,6 @@ def main():
     oil_spill_simulation.oil_movement()
 
     end_time = time.time()  # End the timer
->>>>>>> origin/main
     elapsed_time = end_time - start_time
     print(f"\nExecution time: {elapsed_time:.2f} seconds")
 
